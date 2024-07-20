0c16ebc95bd1f7417671354116670fdd74a7f4cb
Aditya <120248137+adityao007@users.noreply.github.com>
2024-07-20 20:52:32 +0530

    Time: 4 ms (100%), Space: 7.4 MB (25%) - LeetHub

--- 3511-find-the-winning-player-in-coin-game/3511-find-the-winning-player-in-coin-game.cpp
+++ 3511-find-the-winning-player-in-coin-game/3511-find-the-winning-player-in-coin-game.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     string losingPlayer(int x, int y) {
-         // Calculate the number of complete turns that can be made
     int moves = 0;
-
-    // Each move needs at least one 75-value coin and four 10-value coins
     while (x > 0 && y >= 4) {
-        x--; // Use one 75-value coin
-        y -= 4; // Use four 10-value coins
+        x--;
+        y -= 4; 
         moves++;
     }
 
-    // If we can make at least one move, the player making the last move wins
+  
     if (moves % 2 == 1) {
-        return "Alice"; // Odd number of moves, Alice makes the last move
+        return "Alice"; 
     } else {
-        return "Bob"; // Even number of moves, Bob makes the last move
+        return "Bob"; 
     }
     }
 };
