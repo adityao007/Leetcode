2de315c3815e73b76f60d64f0021a5a1f07e15e9
Aditya <120248137+adityao007@users.noreply.github.com>
2024-09-22 09:29:11 +0530

    Time: 841 ms (11.11%), Space: 47.8 MB (11.11%) - LeetHub

--- 3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii/3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii.cpp
+++ 3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii/3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    long long validSubstringCount(std::string word1, std::string word2) {
+    long long validSubstringCount(string word1, string word2) {
         int length1 = word1.length();
         int length2 = word2.length();
 
         if (length2 > length1) return 0;
 
-        std::unordered_map<char, int> charCount1;
-        std::unordered_map<char, int> charCount2;
-        
-        // Populate the frequency map for word2
+        unordered_map<char, int> charCount1;
+        unordered_map<char, int> charCount2;
+
         for (char c : word2) {
             charCount2[c]++;
         }
@@ -23,19 +22,16 @@ public:
             char currentChar = word1[rightPointer];
             charCount1[currentChar]++;
 
-            // Check if the current character matches the required count
             if (charCount2.count(currentChar) && charCount1[currentChar] == charCount2[currentChar]) {
                 formedChars++;
             }
             
-            // When all required characters are formed, count valid substrings
             while (formedChars == requiredChars) {
                 validSubstringCount += length1 - rightPointer;
 
                 char leftChar = word1[leftPointer];
                 charCount1[leftChar]--;
                 
-                // Check if we need to decrease the formed character count
                 if (charCount2.count(leftChar) && charCount1[leftChar] < charCount2[leftChar]) {
                     formedChars--;
                 }
