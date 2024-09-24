54c81bd21f00ae39b080f987090c28fedb17320a
Aditya <120248137+adityao007@users.noreply.github.com>
2024-09-24 11:42:54 +0530

    Time: 87 ms (71.28%), Space: 64.1 MB (41.81%) - LeetHub

--- 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.cpp
@@ -19,18 +19,19 @@ class Solution {
 
  private:
   void inorder(TreeNode* root, vector<int>& nums) {
-    if (root == nullptr)
-      return;
+    if (root == NULL){
+        return;
+    }
     inorder(root->left, nums);
     nums.push_back(root->val);
     inorder(root->right, nums);
   }
 
-  // Same as 108. Convert Sorted Array to Binary Search Tree
-  TreeNode* build(const vector<int>& nums, int l, int r) {
-    if (l > r)
-      return nullptr;
-    const int m = (l + r) / 2;
+  TreeNode* build(vector<int>& nums, int l, int r) {
+    if (l > r){
+        return nullptr;
+    }
+    int m = (l + r) / 2;
     return new TreeNode(nums[m], build(nums, l, m - 1), build(nums, m + 1, r));
   }
 };
