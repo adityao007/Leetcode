a87b9e12c25fd041625a3a1997a282182ff3c2a1
Aditya <120248137+adityao007@users.noreply.github.com>
2024-09-29 08:36:14 +0530

    Time: 4 ms (100%), Space: 10.5 MB (72.73%) - LeetHub

--- 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/3570-count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
+++ 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/3570-count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
@@ -1,62 +1,65 @@
 class Solution {
 public:
-    bool isVowel(char ch) {
+    bool isVowel(char ch){
     return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
 }
-    int countOfSubstrings(string word, int k) {
+    int countOfSubstrings(string word, int k){
         int totalCount = 0;
-    unordered_map<char, int> vowelFreq;
-    int consonantFreq = 0;
-    int start = 0;
+        unordered_map<char, int> vowelFreq;
+        int consonantFreq = 0;
+        int start = 0;
 
-    for (int end = 0; end < word.size(); ++end) {
-        char endChar = word[end];
+        for(int end = 0;end<word.size();end++){
+            char endChar =word[end];
 
-        if (isVowel(endChar)) {
-            vowelFreq[endChar]++;
-        } else {
-            consonantFreq++;
-        }
-
-        while (consonantFreq > k) {
-            char startChar = word[start];
-            if (isVowel(startChar)) {
-                vowelFreq[startChar]--;
-                if (vowelFreq[startChar] == 0) {
-                    vowelFreq.erase(startChar);
-                }
-            } else {
-                consonantFreq--;
+            if(isVowel(endChar)){
+                vowelFreq[endChar]++;
             }
-            start++;
-        }
+            else{
+               consonantFreq++;
+            }
 
-        if (vowelFreq.size() == 5 && consonantFreq == k) {
-            int tempStart = start;
-            while (tempStart <= end && vowelFreq.size() == 5 && consonantFreq == k) {
-                totalCount++;
-                if (isVowel(word[tempStart])) {
-                    vowelFreq[word[tempStart]]--;
-                    if (vowelFreq[word[tempStart]] == 0) {
-                        vowelFreq.erase(word[tempStart]);
-                    }
-                } else {
+            while(consonantFreq > k){
+                char startChar = word[start];
+                if (isVowel(startChar)){
+                    vowelFreq[startChar]--;
+                    if(vowelFreq[startChar] == 0){
+                       vowelFreq.erase(startChar);
+                   }
+                }
+                else{
                     consonantFreq--;
                 }
-                tempStart++;
+                start++;
             }
 
-            while (tempStart > start) {
-                tempStart--;
-                if (isVowel(word[tempStart])) {
-                    vowelFreq[word[tempStart]]++;
-                } else {
-                    consonantFreq++;
+            if(vowelFreq.size() == 5 && consonantFreq == k){
+                int tempStart = start;
+                while(tempStart <= end && vowelFreq.size() == 5 && consonantFreq == k){
+                    totalCount++;
+                    if(isVowel(word[tempStart])){
+                        vowelFreq[word[tempStart]]--;
+                        if(vowelFreq[word[tempStart]] == 0){
+                            vowelFreq.erase(word[tempStart]);
+                        }
+                    }
+                    else{
+                        consonantFreq--;
+                    }
+                    tempStart++;
+                }
+
+                while(tempStart > start){
+                    tempStart--;
+                    if(isVowel(word[tempStart])){
+                        vowelFreq[word[tempStart]]++;
+                    }
+                    else{
+                        consonantFreq++;
+                    }
                 }
             }
         }
-    }
-
-    return totalCount;
+        return totalCount;
     }
 };
