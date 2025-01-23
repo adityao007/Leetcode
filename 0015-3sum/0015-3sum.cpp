08205f8728e48ca020c2a933330f1ad0dbf8ee09
Aditya <120248137+adityao007@users.noreply.github.com>
2025-01-23 17:34:28 +0530

    Time: 51 ms (56.46%), Space: 29 MB (67.1%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,36 +1,39 @@
 class Solution {
- public:
-  vector<vector<int>> threeSum(vector<int>& nums) {
-    if (nums.size() < 3)
-      return {};
-
-    vector<vector<int>> ans;
-
-    ranges::sort(nums);
-
-    for (int i = 0; i + 2 < nums.size(); ++i) {
-      if (i > 0 && nums[i] == nums[i - 1])
-        continue;
-      // Choose nums[i] as the first number in the triplet, then search the
-      // remaining numbers in [i + 1, n - 1].
-      int l = i + 1;
-      int r = nums.size() - 1;
-      while (l < r) {
-        const int sum = nums[i] + nums[l] + nums[r];
-        if (sum == 0) {
-          ans.push_back({nums[i], nums[l++], nums[r--]});
-          while (l < r && nums[l] == nums[l - 1])
-            ++l;
-          while (l < r && nums[r] == nums[r + 1])
-            --r;
-        } else if (sum < 0) {
-          ++l;
-        } else {
-          --r;
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        int n=nums.size();
+        if(n<3){
+            return result;
         }
-      }
-    }
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) {
+                continue;
+            }
+            int left=i+1;
+            int right=n-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum==0){
+                    result.push_back({nums[i],nums[left],nums[right]});
+                    while(left<right && nums[left]==nums[left + 1]){
+                        left++;
+                    }
+                    while(left<right && nums[right]==nums[right-1]){
+                        right--;
+                    }
+                    left++;
+                    right--; 
+                }
+                else if(sum<0){
+                    left++;
+                }else{
+                    right--;
+                }
 
-    return ans;
-  }
+            }
+        }
+        return result;
+    }
 };
