cffa273b4b91cef8853e2560b6a8244c435e249d
Aditya <120248137+adityao007@users.noreply.github.com>
2025-02-11 15:48:12 +0530

    Time: 0 ms (100%), Space: 12.1 MB (8.29%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     string removeOccurrences(string s, string part){
         string ans;
+
         for(int i=0; i<s.length(); i++)
         {
             ans.push_back(s[i]);
