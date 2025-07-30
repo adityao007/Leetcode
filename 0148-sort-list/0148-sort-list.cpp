0943d7a3e20dba2e2f6f0e5a569f0ab623999430
Aditya <120248137+adityao007@users.noreply.github.com>
2025-07-30 16:45:54 +0530

    Time: 47 ms (42.59%), Space: 75.7 MB (21.86%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next){
+            return head;
+        }
+        ListNode* mid=getMiddle(head);
+        ListNode* left=head;
+        ListNode* right=mid->next;
+        mid->next=NULL;
+
+        left=sortList(left);
+        right=sortList(right);
+
+        return merge(left,right);
+            
+    }
+
+private:
+    
+    ListNode* getMiddle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* current = dummy;
+
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                current->next = l1;
+                l1 = l1->next;
+            } else {
+                current->next = l2;
+                l2 = l2->next;
+            }
+            current = current->next;
+        }
+
+        if (l1) current->next = l1;
+        if (l2) current->next = l2;
+
+        return dummy->next; 
+    }
+};
