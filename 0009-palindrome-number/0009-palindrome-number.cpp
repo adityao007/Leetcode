3e0780d50f1c1eed081d7040dab0596553f11f11
Aditya <120248137+adityao007@users.noreply.github.com>
2026-05-13 23:00:55 +0530

    Time: 3 ms (45.36%), Space: 8.6 MB (36.99%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,12 +4,12 @@ public:
         if(x<0 ){
             return false;
         }
-    long reverse = 0;
-    int temp = x;
-    while (temp != 0) {
-        reverse = (reverse * 10) + (temp % 10);
-        temp = temp / 10;
-    }
-    return (reverse == x);
+        long reverse = 0;
+        int temp = x;
+        while (temp != 0) {
+            reverse = (reverse * 10) + (temp % 10);
+            temp = temp / 10;
+        }
+        return (reverse == x);
     }
 };
