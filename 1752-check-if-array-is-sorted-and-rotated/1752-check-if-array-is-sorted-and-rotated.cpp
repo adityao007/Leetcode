77788691f3b06af15c6d0afc3736d1b4c5ed5013
Aditya <120248137+adityao007@users.noreply.github.com>
2026-05-17 17:05:38 +0530

    Time: 0 ms (100%), Space: 11.4 MB (17.99%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -5,7 +5,7 @@ public:
         int rotation =0;
 
         for(int i=0;i<n;i++){
-            if(nums[i]>nums[(i+1)%n] && ++rotation>1){
+            if(nums[i]>nums[(i+1)%n] && rotation++){
                 return false;
             }
         }
